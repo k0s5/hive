ee8a4f5eae1bf0cd19f76f9339409ecc99230dc7
koss <jobs.koss@gmail.com>
2025-06-26 04:17:57 +0900

    feat: Добавление компонента Sidebar с шаблоном и стилями для боковой панели приложения.

---
+++ apps/frontend/client/web/spa/vue/src/widgets/sidebar/ui/Sidebar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <aside class="sidebar">
+    <slot></slot>
+  </aside>
+</template>
+
+<style lang="scss" scoped>
+.sidebar {
+  padding: 0.5rem;
+  width: 400px;
+  height: 100%;
+  background-color: var(--p-surface-ground);
+}
+</style>
