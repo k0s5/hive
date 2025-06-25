26acf55d78dffeb67aaf77aadc9da83ef12db316
koss <jobs.koss@gmail.com>
2025-06-26 04:27:28 +0900

    feat: Добавление компонентов ChatList и ChatWindow с соответствующими шаблонами, стилями и логикой, а также экспортирование их из index.ts.

---
+++ apps/frontend/client/web/spa/vue/src/widgets/chat/chat-list/ui/ChatList.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ChatItem } from '@entities/chat'
+import { chat1Messages, chat2Messages } from '@/shared/mock'
+
+const chat1LastMessage = chat1Messages[chat1Messages.length - 1]
+const chat2LastMessage = chat2Messages[chat2Messages.length - 1]
+
+const chatItems = [
+  {
+    title: 'Иван Петров',
+    lastMessage: chat1LastMessage.text,
+    lastMessageDate: chat1LastMessage.createdAt,
+    unreadMessages: 10,
+  },
+  {
+    title: 'Евгений Сидоров',
+    lastMessage: chat2LastMessage.text,
+    lastMessageDate: chat2LastMessage.createdAt,
+    unreadMessages: 5,
+  },
+]
+</script>
+
+<template>
+  <div class="chat-list">
+    <ChatItem
+      v-for="chatItem of chatItems"
+      :key="chatItem.title"
+      :item="chatItem"
+    />
+  </div>
+</template>
---
+++ apps/frontend/client/web/spa/vue/src/widgets/chat/chat-window/ui/ChatWindow.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ChatHeader } from '@entities/chat'
+import { MessageList, MessageInput } from '@widgets/message'
+import { ref, onMounted } from 'vue'
+
+const chatBodyRef = ref<HTMLDivElement | null>(null)
+
+onMounted(() => {
+  if (chatBodyRef.value) {
+    chatBodyRef.value.scrollTop = chatBodyRef.value.scrollHeight
+  }
+})
+</script>
+
+<template>
+  <div class="chat-window">
+    <ChatHeader />
+    <div ref="chatBodyRef" class="chat-body">
+      <MessageList />
+      <MessageInput />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.chat-window {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: var(--p-card-background);
+}
+.chat-body {
+  padding: 0.5rem;
+  height: calc(100% - 81px);
+  overflow: auto;
+  padding-bottom: 5rem;
+}
+</style>
