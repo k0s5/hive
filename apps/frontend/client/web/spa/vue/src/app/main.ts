5b5b128a6518479b9654b8f778c449d92129e29b
koss <jobs.koss@gmail.com>
2025-06-26 03:18:25 +0900

    feat: Добавление guard пользовательских  маршрутов для проверки авторизации и обновления токенов, а также настройка темы PrimeVue с использованием пользовательского пресета.

--- apps/frontend/client/web/spa/vue/src/app/main.ts
+++ apps/frontend/client/web/spa/vue/src/app/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
+import 'primeicons/primeicons.css'
 import '@shared/styles/index.scss'
 import router from './providers/router'
 import pinia from './providers/store'
 import PrimeVue from 'primevue/config'
+import { definePreset } from '@primeuix/themes'
 import Aura from '@primeuix/themes/aura'
 import App from './App.vue'
 
@@ -10,15 +12,36 @@ const app = createApp(App)
 
 app.use(pinia).use(router)
 
+const customPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{yellow.50}',
+      100: '{yellow.100}',
+      200: '{yellow.200}',
+      300: '{yellow.300}',
+      400: '{yellow.400}',
+      500: '{yellow.500}',
+      600: '{yellow.600}',
+      700: '{yellow.700}',
+      800: '{yellow.800}',
+      900: '{yellow.900}',
+      950: '{yellow.950}',
+    },
+  },
+})
+
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: customPreset,
     options: {
       prefix: 'p',
-      darkModeSelector: 'system',
-      cssLayer: false
-    }
-  }
- })
+      darkModeSelector: '.dark',
+      cssLayer: {
+        name: 'hive',
+        order: 'app-styles, primevue',
+      },
+    },
+  },
+})
 
 app.mount('#app')
--- apps/frontend/client/web/spa/vue/src/app/providers/router.ts
+++ apps/frontend/client/web/spa/vue/src/app/providers/router.ts
@@ -1,20 +1,68 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+  type RouteLocationNormalized,
+  type NavigationGuardNext,
+} from 'vue-router'
+import { useRefreshAuthTokens } from '@/features/auth/refresh/model'
+import useUserStore from '@entities/user/model/store'
+
+const requireAuthGuard = async (
+  _: RouteLocationNormalized,
+  __: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  const userStore = useUserStore()
+  const { refreshAuthTokens } = useRefreshAuthTokens()
+
+  if (!userStore.user) {
+    if (localStorage.getItem('refreshToken')) {
+      await refreshAuthTokens()
+      if (!userStore.user) {
+        return next({ name: 'signin' })
+      }
+      next()
+    } else {
+      next({ name: 'signin' })
+    }
+  }
+  next()
+}
+
+const isAuthorizedGuard = async (
+  _: RouteLocationNormalized,
+  __: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  const userStore = useUserStore()
+  // const { refreshAuthTokens } = useRefreshAuthTokens()
+  // await refreshAuthTokens()
+  if (userStore.user) {
+    next({ name: 'home' })
+  } else {
+    next()
+  }
+}
 
 const routes: readonly RouteRecordRaw[] = [
   {
     path: '/',
     name: 'home',
     component: () => import('../../pages/HomePage.vue'),
+    beforeEnter: requireAuthGuard,
   },
   {
     path: '/signup',
     name: 'signup',
     component: () => import('../../pages/SignupPage.vue'),
+    beforeEnter: isAuthorizedGuard,
   },
   {
     path: '/signin',
     name: 'signin',
     component: () => import('../../pages/SigninPage.vue'),
+    beforeEnter: isAuthorizedGuard,
   },
 ]
 
