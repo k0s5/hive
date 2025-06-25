d09830947058f415df40b8c7b3106ca554d028ef
koss <jobs.koss@gmail.com>
2025-06-26 04:18:29 +0900

    feat: Добавление основного макета приложения с шаблоном и стилями для улучшения структуры страниц.

---
+++ apps/frontend/client/web/spa/vue/src/pages/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="main-layout">
+    <slot name="sidebar"></slot>
+    <main>
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.main-layout {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+main {
+  width: 100%;
+  height: 100%;
+}
+</style>
